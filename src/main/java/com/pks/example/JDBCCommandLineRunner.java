32f4d8622e755e9df778025273e44986e4e294be
Java20091@ <jidedorcas@gmail.com>
2025-03-11 10:44:05 +0100

    deleteing item

---
+++ src/main/java/com/pks/example/JDBCCommandLineRunner.java
@@ -0,0 +1,103 @@
+package com.pks.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JDBCCommandLineRunner implements CommandLineRunner {
+
+
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    Logger log = LoggerFactory.getLogger(JDBCCommandLineRunner.class);
+
+    public JDBCCommandLineRunner(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("In JDBC COMMAND LINE RUNNER");
+        log.info("Truncating Table");
+        jdbcTemplate.execute("truncate table order_details");
+        log.info("Truncating Table ended");
+        
+        insertHardCodedData();
+        insertDataUsingPreparedStatement(2,"sewing-machine","bola","lagos");
+        insertDataUsingNamedJdbcTemplate(3,"marker","bola","lagos");
+        deleteData(2);
+    }
+
+    private void deleteData(int orderId) {
+        System.out.println("deleting order_id " + orderId);
+        String query = """
+                              delete from order_details 
+                              where order_id = :order_id
+                          """;
+        Map param = new HashMap();
+        param.put("order_id",orderId);
+        int rowCount = namedParameterJdbcTemplate.update(query,param);
+        System.out.println("deleted order_id " + orderId);
+    }
+
+    private void insertDataUsingNamedJdbcTemplate(int orderId,
+                                                 String itemName,
+                                                 String customerName,
+                                                             String address) {
+                    String query = """
+                              insert into order_details 
+                              values(:order_id,
+                              :item_name,
+                              :customer_name,
+                              :address)
+                          """;
+        Map params = new HashMap();
+        params.put("order_id",orderId);
+        params.put("item_name",itemName);
+        params.put("customer_name",customerName);
+        params.put("address",address);
+        int rowCount = namedParameterJdbcTemplate.update(query,params);
+    }
+
+    private void insertDataUsingPreparedStatement(int orderId,
+                                                 String itemName,
+                                                 String customerName,
+                                                 String address) {
+      String query = """
+                  insert into order_details values(?,?,?,?)
+              """;
+
+      int rowCount = jdbcTemplate.update(query, new PreparedStatementSetter() {
+          @Override
+          public void setValues(PreparedStatement ps) throws SQLException {
+              ps.setInt(1, orderId);
+              ps.setString(2,itemName);
+              ps.setString(3,customerName);
+              ps.setString(4,address);
+          }
+      });
+
+        System.out.println("Row count " +rowCount);
+
+    }
+
+
+    private void insertHardCodedData() {
+        log.info("Insert beginning");
+        jdbcTemplate.execute("insert into order_details values(1,'pc','jide','sagamu')");
+        log.info("Insert ending");
+    }
+}
