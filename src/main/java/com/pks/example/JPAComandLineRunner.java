2cb3d5794a597b74ae559af639219d5e27b393a5
Java20091@ <jidedorcas@gmail.com>
2025-03-11 17:08:19 +0100

    JPA delete update findby id

--- src/main/java/com/pks/example/JPAComandLineRunner.java
+++ src/main/java/com/pks/example/JPAComandLineRunner.java
@@ -8,6 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @Profile("jpa")
 public class JPAComandLineRunner implements CommandLineRunner {
@@ -18,14 +20,24 @@ public class JPAComandLineRunner implements CommandLineRunner {
     private OrderJpaRepository orderJpaRepository;
     @Override
     public void run(String... args) throws Exception {
+
+
+        System.out.println("Deleting order");
+        orderJpaRepository.deleteAll();
      //  orderRepository.getAllOrder();
       // orderRepository.createAndGetOrder();
         OrderEntity orderEntity = new OrderEntity();
-        orderEntity.setOrderId(11);
+        orderEntity.setOrderId(9);
         orderEntity.setItemName("Brush");
         orderEntity.setCustomerName("tubosun");
         orderEntity.setAddress("sagamu");
         System.out.println("Saving order from JPA");
         orderJpaRepository.save(orderEntity);
+
+        Optional<OrderEntity> orderById = orderJpaRepository.findById(9);
+        System.out.println(orderById.get());
+
+      orderById.get().setAddress("Lagos");
+      orderJpaRepository.save(orderById.get());
     }
 }
